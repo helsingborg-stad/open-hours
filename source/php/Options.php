2b42184eeb8d6a395eba4f4777a0d50a2353db50
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-03-04 16:58:28 +0100

    Info box added to repo.

--- source/php/Options.php
+++ source/php/Options.php
@@ -294,5 +294,50 @@ class Options extends App
         ));
 
         endif;
+
+        if (function_exists('acf_add_local_field_group')):
+
+        acf_add_local_field_group(array(
+            'key' => 'group_56d9a47c90e25',
+            'title' => 'Todays opening hours',
+            'fields' => array(
+                array(
+                    'key' => 'field_56d9a4880cd89',
+                    'label' => 'Shortcode <em>[opening-hours]</em> will print out information displayed below.',
+                    'name' => '',
+                    'type' => 'message',
+                    'instructions' => '',
+                    'required' => 0,
+                    'conditional_logic' => 0,
+                    'wrapper' => array(
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+                    'message' => 'kjhkj',
+                    'new_lines' => '',
+                    'esc_html' => 1,
+                ),
+            ),
+            'location' => array(
+                array(
+                    array(
+                        'param' => 'options_page',
+                        'operator' => '==',
+                        'value' => 'open-hours-settings',
+                    ),
+                ),
+            ),
+            'menu_order' => 0,
+            'position' => 'side',
+            'style' => 'default',
+            'label_placement' => 'top',
+            'instruction_placement' => 'label',
+            'hide_on_screen' => '',
+            'active' => 1,
+            'description' => '',
+        ));
+
+        endif;
     }
 }
